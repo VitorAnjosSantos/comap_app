5d0084e28ce80001fcf466290c047ced6213f008
VitorAnjosSantos <vitor.anjos.santoss@gmail.com>
2019-10-01 16:30:37 -0300

    Criando tela pesquisar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: './pesquisa/pesquisa.module#PesquisaPageModule' },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: 'pesquisa', loadChildren: './pesquisa/pesquisa.module#PesquisaPageModule' }
+  { path: '**', loadChildren: './erro/erro.module#ErroPageModule' }
 
 ];
 @NgModule({
